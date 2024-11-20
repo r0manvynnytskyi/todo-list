26ad5c7671bb81ebe2351bbeb4fc3e769dcb6479
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-20 20:48:34 +0200

    add models to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('content', 'created_at', 'deadline', 'is_done', 'is_overdue')
+    list_filter = ('is_done', 'deadline', 'tags')
+    search_fields = ('content',)
+    filter_horizontal = ('tags',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
