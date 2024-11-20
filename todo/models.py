f5103579d2d8531063b3f86e7484792cc131ef7a
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-20 19:20:43 +0200

    write views for models and change fieldname datetime to created_aet

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Task(models.Model):
     content = models.TextField(max_length=250)
-    datetime = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField('Tag', blank=True)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task, Tag
 
-# Create your views here.
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    model = Task
+    fields = ['content', 'deadline', 'is_done', 'tags']
+    widgets = {
+        'deadline': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+    }
+
+
+# Task List View
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    ordering = ['is_done', '-created_at']
+
+
+# Task Create View
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+# Task Update View
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+# Task Delete View
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
