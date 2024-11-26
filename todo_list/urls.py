260d3d143e444a4b770a3a7a59096b18a3ae4d21
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-26 22:12:16 +0200

    delete all elements from index page and add my own elements

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from todo_list.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from .views import CustomLogoutView
+from .views import (
+    index,
+    TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView,
+    TagListView, TagCreateView, TagUpdateView, TagDeleteView,
+)
+
+app_name = 'todo'
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='task-list'),
+    path('', index, name='index'),
+    path('tasks/', TaskListView.as_view(), name='task-list'),
     path('task/add/', TaskCreateView.as_view(), name='task-add'),
     path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
     path('task/<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
@@ -11,4 +19,5 @@ urlpatterns = [
     path('tag/add/', TagCreateView.as_view(), name='tag-add'),
     path('tag/<int:pk>/update/', TagUpdateView.as_view(), name='tag-update'),
     path('tag/<int:pk>/delete/', TagDeleteView.as_view(), name='tag-delete'),
+    path('logout/', CustomLogoutView.as_view(), name='logout')
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 from django import forms
+from django.views import generic
 from .models import Task, Tag
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 # Task Form
 class TaskForm(forms.ModelForm):
@@ -12,55 +20,82 @@ class TaskForm(forms.ModelForm):
             'deadline': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
 
+
 # Tag Form
 class TagForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = ['name']
 
-# Task List View
-class TaskListView(ListView):
+
+# Task Views
+class TaskListView(generic.ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
     ordering = ['is_done', '-created_at']
 
-# Task Create View
-class TaskCreateView(CreateView):
+
+class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task-list')
 
-# Task Update View
-class TaskUpdateView(UpdateView):
+
+class TaskUpdateView(generic.UpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task-list')
 
-# Task Delete View
-class TaskDeleteView(DeleteView):
+
+class TaskDeleteView(generic.DeleteView):
     model = Task
     template_name = 'tasks/task_confirm_delete.html'
     success_url = reverse_lazy('task-list')
 
-# Tag Create View
-class TagCreateView(CreateView):
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
+
+
+# Tag Views
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = 'tags/tag_list.html'
+    context_object_name = 'tags'
+    ordering = ['name']
+
+
+class TagCreateView(generic.CreateView):
     model = Tag
     form_class = TagForm
     template_name = 'tags/tag_form.html'
     success_url = reverse_lazy('tag-list')
 
-# Tag Update View
-class TagUpdateView(UpdateView):
+
+class TagUpdateView(generic.UpdateView):
     model = Tag
     form_class = TagForm
     template_name = 'tags/tag_form.html'
     success_url = reverse_lazy('tag-list')
 
-# Tag Delete View
-class TagDeleteView(DeleteView):
+
+class TagDeleteView(generic.DeleteView):
     model = Tag
     template_name = 'tags/tag_confirm_delete.html'
     success_url = reverse_lazy('tag-list')
+
+
+class TagDetailView(generic.DetailView):
+    model = Tag
+    template_name = 'tags/tag_detail.html'
+    context_object_name = 'tag'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'includes/logout.html'
+    next_page = reverse_lazy('home')
